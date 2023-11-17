8f777a0b17a8ca74a0f85c3bb266336625e672cc
JeanGReis <106398256+JeanGReis@users.noreply.github.com>
2023-11-17 00:01:05 -0300

    Add files via upload

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,20 +1,26 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, curly_braces_in_flow_control_structures, use_key_in_widget_constructors
 
+import 'dart:ffi';
+
 import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:byte_bargains/meus_widgets.dart';
 import 'package:byte_bargains/styles.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class JogoPage extends StatefulWidget {
   @override
   State<JogoPage> createState() => _JogoPageState();
+  
 }
 
+ 
 class _JogoPageState extends State<JogoPage> {
   // final db = FirebaseFirestore.instance;
   IconData icone = Icons.favorite_outline;
+  final supabase = Supabase.instance.client;
 
   @override
   Widget build(BuildContext context) {
@@ -22,47 +28,86 @@ class _JogoPageState extends State<JogoPage> {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
     if (args.containsKey('nomeJogo')) {
-      nomeJogo = args['nomeJogo'] as String;
+      nomeJogo = args['nomeJogo'] as String;}
+  final User user = supabase.auth.currentUser!;
+    Future<List<dynamic>> pegarListaDesejos() async {
+      final listaDesejos = await supabase
+          .from("Listas de Desejos")
+          .select("jogoId")
+          .eq("idUsuario", user.id);
+      return listaDesejos;
     }
+  
+Future<Map<String, Jogo>> buscarJogosPrincipal(List<String> jogos) async {
+      Map<String, Jogo> jogosFinais = {};
+      final informacoesJogo = await supabase
+          .from("Jogos")
+          .select('''nomeJogo, imagem, descricao''').eq("nomeJogo", 'Alive');
+      final generosJogo = await supabase
+          .from("GenerosJogos")
+          .select('''idGenero, idJogo''').eq("idJogo", 'Alive');
+      final lojasJogo = await supabase
+          .from("LojasPreços")
+          .select('''nomeLoja, precoInicial, desconto, precoFinal, idJogo''').eq(
+              "idJogo", 'Alive');
+      informacoesJogo.forEach((infoJogo) {
+        jogosFinais[infoJogo] = Jogo("", "", "", [], []);
+      });
+      informacoesJogo.forEach((infoJogo) {
+        jogosFinais[infoJogo["nomeJogo"]]!.nome = infoJogo["nomeJogo"];
+        jogosFinais[infoJogo["nomeJogo"]]!.descricao = infoJogo["descricao"];
+        jogosFinais[infoJogo["nomeJogo"]]!.imagem = infoJogo["imagem"];
+      });
+      lojasJogo.forEach(
+        (loja) {
+          jogosFinais[loja["idJogo"]]!.lojas.add(
+            {
+              "loja": {
+                "loja": loja["nomeloja"],
+                "precoInicial": loja["precoInicial"],
+                "desconto": loja["desconto"],
+                "precoFinal": loja["precoFinal"]
+              }
+            },
+          );
+        },
+      );
+      generosJogo.forEach((genero) {
+        jogosFinais[genero["idJogo"]]!.generos.add(genero['idGenero']);
+      });
+      return jogosFinais;
+    }
+
 
     return Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
             top: true,
             child:
-                // StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                //     stream: db.collection("Jogos").doc(nomeJogo).snapshots(),
-                //     builder: (context, snapshot) {
-                //       if (!snapshot.hasData) return CircularProgressIndicator();
-                //       var data = snapshot.data!.data();
-                //       return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                //           stream: db
-                //               .collection("Listas de Desejos")
-                //               .doc(FirebaseAuth.instance.currentUser!.uid)
-                //               .snapshots(),
-                //           builder: (context, snapshot2) {
-                //             if (!snapshot2.hasData)
-                // return CircularProgressIndicator();
-                // var data2 = snapshot2.data!.data();
-                // List<dynamic> jogosDesejados = data2!["Jogos"];
-                // if (jogosDesejados.contains(data!["Nome do Jogo"])) {
-                //   icone = Icons.favorite;
-                // } else {
-                //   icone = Icons.favorite_outline;
-                // }
-                // return
-                Column(
+                FutureBuilder(
+                future: pegarListaDesejos(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) return CircularProgressIndicator();
+                  var data = snapshot.data;
+                  List<String> listaJogosDesejados = [];
+                  data!.forEach((info) => listaJogosDesejados.add(info["jogoId"]));
+                  return FutureBuilder(
+                      future: buscarJogosPrincipal(listaJogosDesejados),
+                      builder: ((context, snapshot) {
+                        if (!snapshot.hasData) return CircularProgressIndicator();
+                        var data2 = snapshot.data;
+              return Column(
               children: [
                 Stack(
                   children: [
                     SizedBox(
                         width: double.infinity,
                         height: 350,
-                        child: Text("Texto PRovisório")
-                        // Image.network(
-                        //   data["Imagem"],
-                        //   fit: BoxFit.fill,
-                        // ),
+                        child:
+                          Image.network(
+                            data2![0]!.imagem,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                     FloatingActionButton(
                       onPressed: () => Navigator.pop(context),
@@ -82,13 +127,11 @@ class _JogoPageState extends State<JogoPage> {
                         child: Column(
                           children: [
                             Text(
-                              "Teste",
-                              // data["Nome do Jogo"],
-                              style: textoNotoSansBoldGrande,
+                              data2[0]!.nome,
+                              style: textoOpenSansRegularPequeno,
                             ),
                             Text(
-                              "Teste",
-                              // data["Descrição"],
+                              data2[0]!.descricao,
                               style: textoOpenSansRegularPequeno,
                             )
                           ],
@@ -106,24 +149,19 @@ class _JogoPageState extends State<JogoPage> {
                         shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20))),
                     onPressed: () {
-                      if (icone == Icons.favorite_outline) {
-                        icone = Icons.favorite;
-                        // jogosDesejados.add(data["Nome do Jogo"]);
-                        // db
-                        //     .collection("Listas de Desejos")
-                        //     .doc(FirebaseAuth
-                        //         .instance.currentUser!.uid)
-                        //     .update({"Jogos": jogosDesejados});
-                      } else {
-                        icone = Icons.favorite_outline;
-                        // jogosDesejados.remove(data["Nome do Jogo"]);
-                        // db
-                        //     .collection("Listas de Desejos")
-                        //     .doc(FirebaseAuth
-                        //         .instance.currentUser!.uid)
-                        //     .update({"Jogos": jogosDesejados});
+                      Future<void> atualizarListaDesejos(String tipo, String jogo) async {
+                        if (tipo == "adicionar"){
+                          await supabase
+                              .from("Listas de Desejos")
+                              .insert({"idUsuario": user.id, "jogoId": jogo});
+                        } else if (tipo == "remover"){
+                          await supabase
+                              .from("Listas de Desejos")
+                              .delete()
+                              .match({"idUsuario": user .id, "jogoId": jogo});
+                        }
                       }
-                      setState(() {});
+                      
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -141,44 +179,46 @@ class _JogoPageState extends State<JogoPage> {
                   ),
                 ),
                 SizedBox(
-                    height: 330, width: 321, child: Text("Texto Provisório")
-                    //  ListView.builder(
-                    //   itemCount: data["Lojas"].length,
-                    //   itemBuilder: (context, index) {
-                    //     return LojaPreco(
-                    //       data["Lojas"][index]['shop'],
-                    //       data["Lojas"][index]['prices']
-                    //                   ["initial_price"] !=
-                    //               "Indisponível"
-                    //           ? data["Lojas"][index]['prices']
-                    //               ["initial_price"]
-                    //           : 0.0,
-                    //       data["Lojas"][index]['prices']["discount"] ==
-                    //               0.0
-                    //           ? Estado.normal
-                    //           : data["Lojas"][index]['prices']
-                    //                       ["discount"] !=
-                    //                   "Indisponível"
-                    //               ? Estado.desconto
-                    //               : Estado.indisponivel,
-                    //       precoDesconto: (data["Lojas"][index]['prices']
-                    //                       ["discount"] ==
-                    //                   0.0 ||
-                    //               data["Lojas"][index]['prices']
-                    //                       ["discount"] ==
-                    //                   "Indisponível")
-                    //           ? 0.0
-                    //           : data["Lojas"][index]['prices']
-                    //               ['final_price'],
-                    //     );
-                    //   },
-                    // ),
+                    height: 330, width: 321, child:                    
+                     ListView.builder(
+                      itemCount: data2[0]!.lojas.length,
+                      itemBuilder: (context, index) {
+                        return LojaPreco(
+                          data2[0]?[index]['shop'],
+                          data2[0][index]['prices']
+                                      ["initial_price"] !=
+                                  "Indisponível"
+                              ? data2[0][index]['prices']
+                                  ["initial_price"]
+                              : 0.0,
+                          data2[0][index]['prices']["discount"] ==
+                                  0.0
+                              ? Estado.normal
+                              : data2[0][index]['prices']
+                                          ["discount"] !=
+                                      "Indisponível"
+                                  ? Estado.desconto
+                                  : Estado.indisponivel,
+                          precoDesconto: (data2[0][index]['prices']
+                                          ["discount"] ==
+                                      0.0 ||
+                                  data2[0][index]['prices']
+                                          ["discount"] ==
+                                      "Indisponível")
+                              ? 0.0
+                              : data2[0][index]['prices']
+                                  ['final_price'],
+                        );
+                      },
+                    ),
                     )
               ],
             )));
+    
   }
   // );
 }
+
 // ),
 // ));
 // }
